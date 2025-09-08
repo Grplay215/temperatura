e7e857510a2c36e48834920e16b5b59922b753d6
GABRIEL RENATO NASCIMENTO MANZOLLA <25203680@CFP127ALUNO.LOCAL>
2025-09-08 16:48:14 -0300

    temperatura é um saco

---
+++ src/temperaturaApp.java
@@ -0,0 +1,9 @@
+public class temperaturaApp {
+
+    public static void main(String[] args) {
+        System.out.println("espere um instante literalmente");
+
+        tempos calor = new tempos();
+        calor.obterDados();
+    }
+}
---
+++ src/tempos.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class tempos {
+
+    double celcios;
+    double kelvin;
+    double Fahrenheit;
+
+    public void obterDados(){
+        Scanner leitor = new Scanner(System.in);
+        System.out.println("quantos graus celcios estão?");
+        celcios = leitor.nextDouble();
+        calculoDeGraus();
+    }
+
+    public void calculoDeGraus(){
+        kelvin = celcios + 273.15;
+
+        Fahrenheit = (celcios * 1.8) + 32;
+
+        exibirResultados();
+    }
+
+    public void exibirResultados(){
+
+        System.out.println("a Temperatura " +celcios+ " °C em Kelvin e em Fahrenheit são respectivamente: ");
+        System.out.println("------------------------");
+        System.out.println(" " +kelvin+ " K");
+        System.out.println("-------------------------");
+        System.out.println(" " +Fahrenheit+ " °F");
+        System.out.println("-------------------------");
+    }
+
+
+}
